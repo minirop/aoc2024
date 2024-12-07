faf44839bd53c77a7a6dda79239e6bb0e44d2231
minirop <minirop@hotmail.com>
2024-12-07 23:32:11 +0100

    day 06: had an infinite loop because of back and forth loops (line instead of square), still doesn't give the correct answer

--- src/day06.rs
+++ src/day06.rs
@@ -89,7 +89,7 @@ pub fn star1() {
 	println!("{steps}", );
 }
 
-pub fn star2_old() {
+pub fn star2() {
 	let content = fs::read_to_string("day06.txt").unwrap();
 	let maze = content.split("\n").collect::<Vec<_>>();
 	let width = maze[0].len();
@@ -99,6 +99,7 @@ pub fn star2_old() {
 	let mut caret = maze.iter().position(|&r| r == b'^').unwrap();
 	let starting_point = caret;
 	maze[caret] = b'.';
+	let maze_copy = maze.clone();
 	let mut direction = NORTH;
 
 	let mut tests = HashSet::new();
@@ -170,14 +171,15 @@ pub fn star2_old() {
 	}
 
 	let mut total = 0;
-	// println!("steps: {}", tests.len());
+	let max_steps = height*width;
 	for test in tests {
-		// println!("doing test: {test}", );
 		let mut visited = HashMap::new();
 
 		caret = starting_point;
 		direction = NORTH;
+		let mut maze = maze_copy.clone();
 		maze[test] = b'#';
+		let mut step_taken = 0;
 		loop {
 			let next_pos = match direction {
 				NORTH => {
@@ -238,10 +240,16 @@ pub fn star2_old() {
 				},
 				_ => todo!(), 
 			};
+			step_taken += 1;
+
+			// infinite loop detected
+			if step_taken > max_steps {
+				total += 1;
+				break;
+			}
 
 			if let Some(already) = visited.get(&next_pos) {
 				if *already == direction {
-					// panic!("FOUND!!!");
 					total += 1;
 					break;
 				}
@@ -249,196 +257,6 @@ pub fn star2_old() {
 			visited.insert(next_pos, direction);
 			caret = next_pos;
 		}
-
-		// for y in 0..width {
-		// 	for x in 0..height {
-		// 		print!("{}", maze[x + y * width] as char);
-		// 	}
-		// 	println!("");
-		// }
-		// println!("{:?}", visited);
-		// println!("");
-
-		maze[test] = b'.';
-	}
-
-	println!("{total}");
-}
-
-pub fn star2() {
-	let content = fs::read_to_string("day06.txt").unwrap();
-	let maze = content.split("\n").collect::<Vec<_>>();
-	let width = maze[0].len();
-	let height = maze.len();
-	let mut maze = maze.join("").as_bytes().to_vec();
-
-	let mut caret = maze.iter().position(|&r| r == b'^').unwrap();
-	let starting_point = caret;
-	maze[caret] = b'.';
-	let mut direction = NORTH;
-
-	let mut tests = HashSet::new();
-	tests.insert(caret);
-
-	loop {
-		let next_pos = match direction {
-			NORTH => {
-				let (next_pos, ov) = caret.overflowing_sub(width);
-				if ov { break; }
-
-				if maze[next_pos] == b'#' {
-					direction = EAST;
-					caret
-				} else {
-					next_pos
-				}
-			},
-			EAST => {
-				let (next_pos, ov) = caret.overflowing_add(1);
-				if ov { break; }
-
-				let guard_y_prev = caret / width;
-				let guard_y_next = next_pos / width;
-				if guard_y_prev != guard_y_next {
-					break;
-				}
-
-				if maze[next_pos] == b'#' {
-					direction = SOUTH;
-					caret
-				} else {
-					next_pos
-				}
-			},
-			SOUTH => {
-				let (next_pos, ov) = caret.overflowing_add(width);
-				if ov || next_pos >= maze.len() { break; }
-
-				if maze[next_pos] == b'#' {
-					direction = WEST;
-					caret
-				} else {
-					next_pos
-				}
-			},
-			WEST => {
-				let (next_pos, ov) = caret.overflowing_sub(1);
-				if ov { break; }
-
-				let guard_y_prev = caret / width;
-				let guard_y_next = next_pos / width;
-				if guard_y_prev != guard_y_next {
-					break;
-				}
-
-				if maze[next_pos] == b'#' {
-					direction = NORTH;
-					caret
-				} else {
-					next_pos
-				}
-			},
-			_ => todo!(), 
-		};
-
-		tests.insert(next_pos);
-		caret = next_pos;
-	}
-
-	let mut total = 0;
-	// println!("steps: {}", tests.len());
-	for test in tests {
-		// println!("doing test: {test}", );
-		let mut maze = maze.clone();
-		thread::spawn(move || {
-			let mut visited = HashMap::new();
-
-			caret = starting_point;
-			direction = NORTH;
-			maze[test] = b'#';
-			loop {
-				let next_pos = match direction {
-					NORTH => {
-						let (next_pos, ov) = caret.overflowing_sub(width);
-						if ov { break; }
-
-						if maze[next_pos] == b'#' {
-							direction = EAST;
-							caret
-						} else {
-							next_pos
-						}
-					},
-					EAST => {
-						let (next_pos, ov) = caret.overflowing_add(1);
-						if ov { break; }
-
-						let guard_y_prev = caret / width;
-						let guard_y_next = next_pos / width;
-						if guard_y_prev != guard_y_next {
-							break;
-						}
-
-						if maze[next_pos] == b'#' {
-							direction = SOUTH;
-							caret
-						} else {
-							next_pos
-						}
-					},
-					SOUTH => {
-						let (next_pos, ov) = caret.overflowing_add(width);
-						if ov || next_pos >= maze.len() { break; }
-
-						if maze[next_pos] == b'#' {
-							direction = WEST;
-							caret
-						} else {
-							next_pos
-						}
-					},
-					WEST => {
-						let (next_pos, ov) = caret.overflowing_sub(1);
-						if ov { break; }
-
-						let guard_y_prev = caret / width;
-						let guard_y_next = next_pos / width;
-						if guard_y_prev != guard_y_next {
-							break;
-						}
-
-						if maze[next_pos] == b'#' {
-							direction = NORTH;
-							caret
-						} else {
-							next_pos
-						}
-					},
-					_ => todo!(), 
-				};
-
-				if let Some(already) = visited.get(&next_pos) {
-					if *already == direction {
-						// panic!("FOUND!!!");
-						total += 1;
-						break;
-					}
-				}
-				visited.insert(next_pos, direction);
-				caret = next_pos;
-			}
-		});
-
-		// for y in 0..width {
-		// 	for x in 0..height {
-		// 		print!("{}", maze[x + y * width] as char);
-		// 	}
-		// 	println!("");
-		// }
-		// println!("{:?}", visited);
-		// println!("");
-
-		// maze[test] = b'.';
 	}
 
 	println!("{total}");
